c35fe14317ad18aa618b52e02f65451939baa8e2
Awanish kumar singh <awanishkumarsingh03@gmail.com>
2021-06-22 17:29:59 +0530

    the life cycle method was implemented using the interface the second method.

---
+++ com.Springcore/src/main/java/com/Springcore/lifecycle/Test.java
@@ -0,0 +1,29 @@
+package com.Springcore.lifecycle;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+
+	public static void main(String[] aar) {
+
+		AbstractApplicationContext context = new ClassPathXmlApplicationContext("com/Springcore/lifecycle/config.xml");
+
+		Samosa s = (Samosa)context.getBean("samosa");
+		
+		System.out.println(s);
+		context.registerShutdownHook();
+		
+		System.out.println("+++++++++++++++++++++++++++++++++++");
+		
+		Pepsi pepsi = (Pepsi)context.getBean("soft");
+		
+		System.out.println(pepsi);
+		
+		
+		
+		
+	}
+
+}
