74307e8ea3ae9d41fb17ce1739e333c6f46de070
Awanish kumar singh <awanishkumarsingh03@gmail.com>
2021-06-25 10:57:37 +0530

    adding a new field in a correct way

--- com.Springcore/src/main/java/com/Springcore/spl/methodcall/Demo.java
+++ com.Springcore/src/main/java/com/Springcore/spl/methodcall/Demo.java
@@ -15,9 +15,13 @@ public class Demo {
 	private double e;
 	@Value("#{ T(java.lang.Math).sqrt(144) }")
 	private int square;
+	@Value("true")
+	private boolean call;
+	
 	@Override
 	public String toString() {
-		return "Demo [a=" + a + ", name=" + name + ", pi=" + pi + ", e=" + e + ", square=" + square + "]";
+		return "Demo [a=" + a + ", name=" + name + ", pi=" + pi + ", e=" + e + ", square=" + square + ", call=" + call
+				+ "]";
 	}
 	
 	
