54aac0aa42cc8feb60e85e22544cf83668780ceb
Awanish kumar singh <awanishkumarsingh03@gmail.com>
2021-06-25 10:45:10 +0530

    calling the static method is done

---
+++ com.Springcore/src/main/java/com/Springcore/spl/methodcall/Demo.java
@@ -0,0 +1,25 @@
+package com.Springcore.spl.methodcall;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component("maths")
+public class Demo {
+	@Value("10")
+	private int a;
+	@Value("Maths")
+	private String name;
+	@Value("#{T(java.lang.Math).PI}")
+	private double pi;
+	@Value("#{ T(java.lang.Math).E }")
+	private double e;
+	@Value("#{ T(java.lang.Math).sqrt(144) }")
+	private int square;
+	@Override
+	public String toString() {
+		return "Demo [a=" + a + ", name=" + name + ", pi=" + pi + ", e=" + e + ", square=" + square + "]";
+	}
+	
+	
+
+}
